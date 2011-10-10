8001ba6f7f4fa0112eba52b7ec48133277dc81e4
Thomas <tjom@gmx.at>
2011-10-10 20:54:49 +0200

    [enh] TextView for help text + close button
    Signed-off-by: Thomas <tjom@gmx.at>

--- src/NoNoise/Banshee.NoNoise/NoNoiseHelpDialog.cs
+++ src/NoNoise/Banshee.NoNoise/NoNoiseHelpDialog.cs
@@ -32,26 +32,68 @@ namespace Banshee.NoNoise
 {
     public class NoNoiseHelpDialog : Gtk.Dialog
     {
+        private readonly string general_help =
+@"Help text!
+
+This is going to be the help text!
+NoNoise general help text. NoNoise general help text. NoNoise general help text. NoNoise general help text.
+";
+        private readonly string specific_help =
+@"Help text!
+
+This is going to be the specific help text!
+NoNoise specific help text. NoNoise specific help text. NoNoise specific help text. NoNoise specific help text.
+";
 
         public NoNoiseHelpDialog ()
         {
             Notebook notebook = new Notebook ();
             VBox box1 = new VBox ();
-            Label text1 = new Label ("NoNoise general help text. NoNoise general help text. NoNoise general help text.");
-            box1.PackStart (text1, true, true, 5);
+            TextView tv1 = new TextView ();
+            tv1.Editable = false;
+            tv1.CursorVisible = false;
+//            tv.Sensitive = false;
+            TextBuffer tb1 = new TextBuffer (new TextTagTable ());
+            tb1.Text = general_help;
+            tv1.Buffer = tb1;
+            tv1.WrapMode = WrapMode.Word;
+//            tv.SetSizeRequest (460, 380);
+            ScrolledWindow sw1 = new ScrolledWindow ();
+            sw1.AddWithViewport (tv1);
+            sw1.SetSizeRequest (460, 380);
+            box1.PackStart (sw1, true, true, 5);
             notebook.AppendPage (box1, new Label ("General"));
 
             VBox box2 = new VBox ();
-            Label text2 = new Label ("NoNoise specific help text. NoNoise specific help text. NoNoise specific help text.");
-            box2.PackStart (text2, true, true, 5);
+            TextView tv2 = new TextView ();
+            tv2.Editable = false;
+            tv2.CursorVisible = false;
+            TextBuffer tb2 = new TextBuffer (new TextTagTable ());
+            tb2.Text = specific_help;
+            tv2.Buffer = tb2;
+            tv2.WrapMode = WrapMode.Word;
+            ScrolledWindow sw2 = new ScrolledWindow ();
+            sw2.AddWithViewport (tv2);
+            sw2.SetSizeRequest (460, 380);
+            box2.PackStart (sw2, true, true, 5);
             notebook.AppendPage (box2, new Label ("Specific"));
+
+            Button close_button = new Button ();
+            close_button.Label = "Close";
+            close_button.Clicked += new EventHandler (OnCloseButtonClicked);
 
             Title = "NoNoise Help";
             VBox.PackStart (notebook, true, true, 5);
-            SetDefaultSize (240, 180);
+            AddActionWidget (close_button, 0);
+            SetDefaultSize (460, 380);
             Resizable = false;
 
             ShowAll ();
+        }
+
+        private void OnCloseButtonClicked (object sender, EventArgs e)
+        {
+            Destroy ();
         }
     }
 }
